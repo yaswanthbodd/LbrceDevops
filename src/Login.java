1cbe8a723f052f6de866399574df62bd962d5f1d
AdmiN <AdmiN@ITWS011>
2024-08-21 15:09:27 +0530

    Login Page

---
+++ src/Login.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Please Enter Login Credentials");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Your Email:");
+		String email=sc.next();
+		System.out.println("Enter Password: ");
+		String pass=sc.next();
+		System.out.println("Login Successfull..");
+		
+	}
+
+}
--- src/Register.java
+++ src/Register.java
@@ -4,15 +4,20 @@ public class Register {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int RollNo;
 		System.out.println("Register Form ");
 		System.out.println("Enter Your Roll Number: ");
-		RollNo=sc.nextInt();
+		int RollNo=sc.nextInt();
 		System.out.println("Enter Your Name: ");
 		String s=sc.next();
+		System.out.println("Enter Your Email: ");
+		String email=sc.next();
 		System.out.println("Enter your Password: ");
 		String pass=sc.next();
-		System.out.println("Roll No: "+RollNo+" Name: "+s+" Password: "+pass);
+		if(RollNo !=0 && s!="" && email!="" && pass!="") {
+			System.out.println("Registration Successfully..");
+		}else {
+			System.out.println("Registration Not Successfull. Please fill all the rows");
+		}
 	}
 
 }
